4d7ba24f337953fa5a4373e35d6737d7a7048030
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-08-12 11:36:58 +0100

    Create bar class

---
+++ specs/bar_specs.rb
@@ -0,0 +1,28 @@
+require("minitest/autorun")
+require("minitest/rg")
+require_relative("../bar.rb")
+require_relative("../guest.rb")
+require_relative("../drink.rb")
+
+class TestBar < MiniTest::Test
+  def setup
+    @guest1 = Guest.new("Jack", 20, "Run to the hills")
+    @guest2 = Guest.new("John", 10, "The Trooper")
+    @guest3 = Guest.new("Melinda", 5, "Korsakov")
+    @guest4 = Guest.new("Melody", 30, "Macarena")
+
+    @drink1 = Drink.new("Beer", 2, 10)
+    @drink2 = Drink.new("Wine", 5, 0)
+    @drink3 = Drink.new("MegaCocktail", 20, 2)
+
+    @drinks = [@drink1, @drink2, @drink3]
+
+    @bar = Bar.new(0, @drinks)
+  end
+
+  def test_has_till
+  end
+
+  def test_has_stock
+  end
+end
